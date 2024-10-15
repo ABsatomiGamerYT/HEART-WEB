c935fce26e93fad5ac94d4e31776ba4139355a39
春木 | 𝕳𝖆𝖗𝖚𝖐𝖎 <54508175+ABsatomiGamerYT@users.noreply.github.com>
2024-10-15 09:09:26 -0500

    Add files via upload

--- java/galeria-modal.js
+++ java/galeria-modal.js
@@ -1,16 +1,25 @@
-// Obtén el modal
+// Obtener el modal y el contenido de la imagen
 var modal = document.getElementById("myModal");
-
-// Obtén la imagen y el texto del caption
-var img = document.getElementsByClassName("modal-img");
 var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+var images = document.getElementsByClassName("imagen-port");
+var closeBtn = document.getElementsByClassName("close")[0];
 
-// Agrega el evento a cada imagen
-for (let i = 0; i < img.length; i++) {
-    img[i].onclick = function(){
+// Agregar el evento de click a cada imagen para abrir el modal
+for (var i = 0; i < images.length; i++) {
+    images[i].onclick = function() {
         modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.nextElementSibling.innerHTML; // Toma el texto del caption
+        modalImg.src = this.querySelector('img').src; // Tomar la fuente de la imagen dentro del contenedor .imagen-port
+    }
+}
+
+// Evento de cierre del modal cuando se presiona el botón "X"
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+// También puedes permitir cerrar el modal al hacer clic fuera de la imagen
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
     }
 }
